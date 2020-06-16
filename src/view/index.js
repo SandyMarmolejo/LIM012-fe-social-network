5f1350c9ae6b6ae257dd3d1288ed3870695bff77
Erikaquispenjanampa <erikaquispejanampa@gmail.com>
2020-06-16 09:44:13 -0500

    Actualización del login y register

--- src/view/index.js
+++ src/view/index.js
@@ -1,7 +1,7 @@
 
 // Creando un objeto, con propiedad home y ahi vamos a poner las vistas o componentes
-import Login from './login2.js';
-import Register from './register2.js';
+import Login from './login.js';
+import Register from './register.js';
 import Home from './home.js';
 import NotFound from './notFound.js';
 
--- src/view/register.js
+++ src/view/register.js
@@ -1,39 +1,37 @@
 const register = () => {
   const viewRegister = `
-  <h1 class="title-logo">TRAVEL OF WORLD</h1>
-  <main class="access-register-container">
-   
-    <form id="formRegister" class="form-register">
-        <p>
-            <input type="text" id="txtNameSignUp" class="name-register" placeholder="Nombre y Apellido" required/>
-        </p>
-        <p>
-            <input type="text" id="txtEmailSignUp" class="email-register" placeholder="Email" required/>
-        </p>
-        <p>
-            <input type="text" id="txtPasswordSignUp" class="password-register" placeholder="Contraseña" required/>
-        </p>
-        <p>
-            <input type="checkbox" value="Terminos" /> <label>Acepto términos, condiciones y política de privacidad</label>
-        </p>
-        <button href="#/register" id="btnRegister" class="btn-register">Regístrate</button>
-    </form>
-
-    <div class="question-container">
-    <p class="message-question">¿Ya tienes una cuenta?</p>
-    <a href="#/login" id="btnViewLogIn" class="txt-init-session">Inicia sesión</a>
+  <h2>Travel of Wordl</h2>
+  <form id="formRegister">
+    <input id="txtNameSignUp" type="text" class="user active" value="Nombre y Apellido" onfocus="this.value = &#39;&#39;;" onblur="if (this.value == &#39;&#39;) {this.value = &#39;Nombre y Apellido&#39;;}">
+    <input id="txtEmailSignUp" type="text" class="user active" value="Email" onfocus="this.value = &#39;&#39;;" onblur="if (this.value == &#39;&#39;) {this.value = &#39;Email&#39;;}">
+    <input id="txtPasswordSignUp" type="password" class="lock active" value="Contraseña" onfocus="this.value = &#39;&#39;;" onblur="if (this.value == &#39;&#39;) {this.value = &#39;Contraseña&#39;;}">
+    <input type="checkbox" value="Terminos" /> 
+      <label>Acepto términos, condiciones y política de privacidad</label>
+    <div class="login-bwn">
+        <input type="submit" value="Regístrate" id="btnRegister">
     </div>
-</main>
+  </form>
+
+  <div class="login-bottom">
+   
+    <div class="social-icons">
+      <h4> <a href="#">¿Ya tienes una cuenta?</a></h4>
+      <div class="reg-bwn">
+        <a href="#/login" id="btnViewSignUp">Inicia sesión</a>
+      </div>
+    </div>
+  </div>
 `;
   // Creando un elemento nodo de tipo div
   const divRegister = document.createElement('div');
   // Añadimos una cadena de texto
   divRegister.innerHTML = viewRegister;
+   divRegister.classList.add('login');
 
   // Creando una cuenta de usuario
   const formRegister = divRegister.querySelector('#formRegister');
-
   formRegister.addEventListener('submit', () => {
+
   });
 
   return divRegister;
